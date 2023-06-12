719772fb42650f6dcaec176ca7a9d3dc26a98513
Siavash Hekmatnia <siavash.hekmatnia@gmail.com>
2023-06-12 13:37:04 +0330

    added implementation layouts for events that order service is going to listen for

---
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/PaymentResponseMessageListenerImpl.java
@@ -0,0 +1,16 @@
+package com.food.ordering.system.order.service.domain;
+
+import com.food.ordering.system.order.service.domain.dto.message.PaymentResponse;
+import com.food.ordering.system.order.service.domain.ports.input.message.listener.payment.PaymentResponseMessageListener;
+
+public class PaymentResponseMessageListenerImpl implements PaymentResponseMessageListener {
+    @Override
+    public void paymentCompleted(PaymentResponse paymentResponse) {
+
+    }
+
+    @Override
+    public void paymentCancelled(PaymentResponse paymentResponse) {
+
+    }
+}
---
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/RestaurantApprovalResponseMessageListenerImpl.java
@@ -0,0 +1,16 @@
+package com.food.ordering.system.order.service.domain;
+
+import com.food.ordering.system.order.service.domain.dto.message.RestaurantApprovalResponse;
+import com.food.ordering.system.order.service.domain.ports.input.message.listener.restauarantapproval.RestaurantApprovalResponseMessageListener;
+
+public class RestaurantApprovalResponseMessageListenerImpl implements RestaurantApprovalResponseMessageListener {
+    @Override
+    public void orderApproved(RestaurantApprovalResponse restaurantApprovalResponse) {
+
+    }
+
+    @Override
+    public void orderRejected(RestaurantApprovalResponse restaurantApprovalResponse) {
+
+    }
+}
