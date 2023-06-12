729154992d08f4b5af328979fd7b1a30c9ce632b
Siavash Hekmatnia <siavash.hekmatnia@gmail.com>
2023-06-12 13:35:40 +0330

    order tracking by trackingId

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/input/message/listener/restauarantapproval/RestaurantApprovalResponseMessageListener.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/input/message/listener/restauarantapproval/RestaurantApprovalResponseMessageListener.java
@@ -2,7 +2,7 @@ package com.food.ordering.system.order.service.domain.ports.input.message.listen
 
 import com.food.ordering.system.order.service.domain.dto.message.RestaurantApprovalResponse;
 
-public interface RestarantApprovalResponseMessageListener {
+public interface RestaurantApprovalResponseMessageListener {
 
     void orderApproved(RestaurantApprovalResponse restaurantApprovalResponse);
 
