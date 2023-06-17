11dad6e2bfd27623546358e130874219e7629a35
Siavash Hekmatnia <siavash.hekmatnia@gmail.com>
2023-06-17 10:20:24 +0330

    implemented restaurant dataaccess

---
+++ order-service/order-dataaccess/src/main/java/com/food/ordering/system/order/service/dataaccess/restaurant/exception/RestaurantDataAccessException.java
@@ -0,0 +1,11 @@
+package com.food.ordering.system.order.service.dataaccess.restaurant.exception;
+
+public class RestaurantDataAccessException extends RuntimeException {
+    public RestaurantDataAccessException(String message) {
+        super(message);
+    }
+
+    public RestaurantDataAccessException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
