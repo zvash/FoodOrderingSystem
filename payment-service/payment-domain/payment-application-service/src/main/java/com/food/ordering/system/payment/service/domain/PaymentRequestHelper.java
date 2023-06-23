595e6e9441116a4b5bda3912ed0a739dcd729a22
Siavash Hekmatnia <siavash.hekmatnia@gmail.com>
2023-06-23 17:06:23 +0330

    implemented PaymentRequestHelper to create new payments or cancel previous payments

---
+++ payment-service/payment-domain/payment-application-service/src/main/java/com/food/ordering/system/payment/service/domain/PaymentRequestHelper.java
@@ -0,0 +1,119 @@
+package com.food.ordering.system.payment.service.domain;
+
+import com.food.ordering.system.domain.valueobject.CustomerId;
+import com.food.ordering.system.domain.valueobject.OrderId;
+import com.food.ordering.system.payment.service.domain.dto.PaymentRequest;
+import com.food.ordering.system.payment.service.domain.entity.CreditEntry;
+import com.food.ordering.system.payment.service.domain.entity.CreditHistory;
+import com.food.ordering.system.payment.service.domain.entity.Payment;
+import com.food.ordering.system.payment.service.domain.event.PaymentEvent;
+import com.food.ordering.system.payment.service.domain.exception.PaymentApplicationServiceException;
+import com.food.ordering.system.payment.service.domain.mapper.PaymentDataMapper;
+import com.food.ordering.system.payment.service.domain.ports.output.repository.CreditEntryRepository;
+import com.food.ordering.system.payment.service.domain.ports.output.repository.CreditHistoryRepository;
+import com.food.ordering.system.payment.service.domain.ports.output.repository.PaymentRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Slf4j
+@Component
+public class PaymentRequestHelper {
+
+    private final PaymentDomainService paymentDomainService;
+    private final PaymentDataMapper paymentDataMapper;
+    private final PaymentRepository paymentRepository;
+    private final CreditEntryRepository creditEntryRepository;
+    private final CreditHistoryRepository creditHistoryRepository;
+
+    public PaymentRequestHelper(PaymentDomainService paymentDomainService, PaymentDataMapper paymentDataMapper,
+                                PaymentRepository paymentRepository, CreditEntryRepository creditEntryRepository,
+                                CreditHistoryRepository creditHistoryRepository) {
+        this.paymentDomainService = paymentDomainService;
+        this.paymentDataMapper = paymentDataMapper;
+        this.paymentRepository = paymentRepository;
+        this.creditEntryRepository = creditEntryRepository;
+        this.creditHistoryRepository = creditHistoryRepository;
+    }
+
+    @Transactional
+    public PaymentEvent persistPayment(PaymentRequest paymentRequest) {
+        log.info("Received payment complete event for order id: {}", paymentRequest.getOrderId());
+        Payment payment = paymentDataMapper.paymentRequestModelToPayment(paymentRequest);
+        CreditEntry creditEntry = gatCreditEntry(payment.getCustomerId());
+        List<CreditHistory> creditHistories = getCreditHistories(payment.getCustomerId());
+        List<String> failureMessages = new ArrayList<>();
+        PaymentEvent paymentEvent = paymentDomainService.validateAndInitiatePayment(
+                payment,
+                creditEntry,
+                creditHistories,
+                failureMessages
+        );
+        persistDbObjects(payment, creditEntry, creditHistories, failureMessages);
+        return paymentEvent;
+    }
+
+    @Transactional
+    public PaymentEvent persistCancelPayment(PaymentRequest paymentRequest) {
+        log.info("Received payment rollback event for order id: {}", paymentRequest.getOrderId());
+        Payment payment = getPaymentFromPaymentRequest(paymentRequest);
+        CreditEntry creditEntry = gatCreditEntry(payment.getCustomerId());
+        List<CreditHistory> creditHistories = getCreditHistories(payment.getCustomerId());
+        List<String> failureMessages = new ArrayList<>();
+        PaymentEvent paymentEvent = paymentDomainService.validateAndCancelPayment(
+                payment,
+                creditEntry,
+                creditHistories,
+                failureMessages
+        );
+        persistDbObjects(payment, creditEntry, creditHistories, failureMessages);
+        return paymentEvent;
+    }
+
+    private CreditEntry gatCreditEntry(CustomerId customerId) {
+        Optional<CreditEntry> creditEntry = creditEntryRepository.findByCustomerId(customerId);
+        if (creditEntry.isEmpty()) {
+            log.error("Could not find credit entry for customer: {}", customerId.getValue());
+            throw new PaymentApplicationServiceException("Could not find credit entry for customer: " +
+                    customerId.getValue());
+        }
+        return creditEntry.get();
+    }
+
+    private List<CreditHistory> getCreditHistories(CustomerId customerId) {
+        Optional<List<CreditHistory>> creditHistories = creditHistoryRepository.findByCustomerId(customerId);
+        if (creditHistories.isEmpty()) {
+            log.error("Could not find credit history for customer: {}", customerId.getValue());
+            throw new PaymentApplicationServiceException("Could not find credit history for customer: " +
+                    customerId.getValue());
+        }
+        return creditHistories.get();
+    }
+
+    private Payment getPaymentFromPaymentRequest(PaymentRequest paymentRequest) {
+        OrderId orderId = new OrderId(UUID.fromString(paymentRequest.getOrderId()));
+        Optional<Payment> optionalPayment = paymentRepository.findByOrderId(orderId);
+        if (optionalPayment.isEmpty()) {
+            log.error("Payment with order id: {} could not be found!", paymentRequest.getOrderId());
+            throw new PaymentApplicationServiceException("Payment with order id: " +
+                    paymentRequest.getOrderId() + " could not be  found!");
+        }
+        return optionalPayment.get();
+    }
+
+    private void persistDbObjects(Payment payment,
+                                 CreditEntry creditEntry,
+                                 List<CreditHistory> creditHistories,
+                                 List<String> failureMessages) {
+        paymentRepository.save(payment);
+        if (failureMessages.isEmpty()) {
+            creditEntryRepository.save(creditEntry);
+            creditHistoryRepository.save(creditHistories.get(creditHistories.size() - 1));
+        }
+    }
+}
--- payment-service/payment-domain/payment-application-service/src/main/java/com/food/ordering/system/payment/service/domain/mapper/PaymentDataMapper.java
+++ payment-service/payment-domain/payment-application-service/src/main/java/com/food/ordering/system/payment/service/domain/mapper/PaymentDataMapper.java
@@ -1,7 +1,13 @@
 package com.food.ordering.system.payment.service.domain.mapper;
 
+import com.food.ordering.system.domain.valueobject.CustomerId;
+import com.food.ordering.system.domain.valueobject.Money;
+import com.food.ordering.system.domain.valueobject.OrderId;
+import com.food.ordering.system.payment.service.domain.dto.PaymentRequest;
+import com.food.ordering.system.payment.service.domain.entity.Payment;
 import org.springframework.stereotype.Component;
 
+import java.util.UUID;
 
 /**
  * This class will create domain objects from input objects
@@ -11,4 +17,11 @@ import org.springframework.stereotype.Component;
 public class PaymentDataMapper {
 
 
+    public Payment paymentRequestModelToPayment(PaymentRequest paymentRequest) {
+        return Payment.Builder.builder()
+                .orderId(new OrderId(UUID.fromString(paymentRequest.getOrderId())))
+                .customerId(new CustomerId(UUID.fromString(paymentRequest.getCustomerId())))
+                .price(new Money(paymentRequest.getPrice()))
+                .build();
+    }
 }
