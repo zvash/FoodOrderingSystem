0c8d1e801b9c2c70f7a2c48dc2f6a421edcb6e7b
Siavash Hekmatnia <siavash.hekmatnia@gmail.com>
2023-06-23 15:23:37 +0330

    added PaymentRequest dto to accept payment request messages from other services

---
+++ payment-service/payment-domain/payment-application-service/src/main/java/com/food/ordering/system/payment/service/domain/mapper/PaymentDataMapper.java
@@ -0,0 +1,14 @@
+package com.food.ordering.system.payment.service.domain.mapper;
+
+import org.springframework.stereotype.Component;
+
+
+/**
+ * This class will create domain objects from input objects
+ * and created output objects from domain objects
+ */
+@Component
+public class PaymentDataMapper {
+
+
+}
