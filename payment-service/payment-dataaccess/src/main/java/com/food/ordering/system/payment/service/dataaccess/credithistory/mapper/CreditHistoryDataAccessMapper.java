88ba1a0fb67795cab82e19a53d6de6bcc5313875
Siavash Hekmatnia <siavash.hekmatnia@gmail.com>
2023-06-25 00:41:02 +0330

    implemented the credit history entity jpa codes in payment data access module

---
+++ payment-service/payment-dataaccess/src/main/java/com/food/ordering/system/payment/service/dataaccess/credithistory/mapper/CreditHistoryDataAccessMapper.java
@@ -0,0 +1,38 @@
+package com.food.ordering.system.payment.service.dataaccess.credithistory.mapper;
+
+import com.food.ordering.system.domain.valueobject.CustomerId;
+import com.food.ordering.system.domain.valueobject.Money;
+import com.food.ordering.system.payment.service.dataaccess.credithistory.entity.CreditHistoryEntity;
+import com.food.ordering.system.payment.service.domain.entity.CreditHistory;
+import com.food.ordering.system.payment.service.domain.valueobject.CreditHistoryId;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class CreditHistoryDataAccessMapper {
+
+    public CreditHistoryEntity creditHistoryToCreditHistoryEntity(CreditHistory creditHistory) {
+        return CreditHistoryEntity.builder()
+                .id(creditHistory.getId().getValue())
+                .customerId(creditHistory.getCustomerId().getValue())
+                .amount(creditHistory.getAmount().getAmount())
+                .transactionType(creditHistory.getTransactionType())
+                .build();
+    }
+
+    public CreditHistory creditHistoryEntityToCreditHistory(CreditHistoryEntity creditHistoryEntity) {
+        return CreditHistory.Builder.builder()
+                .id(new CreditHistoryId(creditHistoryEntity.getId()))
+                .customerId(new CustomerId(creditHistoryEntity.getCustomerId()))
+                .amount(new Money(creditHistoryEntity.getAmount()))
+                .transactionType(creditHistoryEntity.getTransactionType())
+                .build();
+    }
+
+    public List<CreditHistory> creditHistoryEntitiesToCreditHistories(List<CreditHistoryEntity> creditHistoryEntities) {
+        return creditHistoryEntities.stream()
+                .map(this::creditHistoryEntityToCreditHistory)
+                .toList();
+    }
+}
