5f2b35260309f5968a5cf0917899afc224c07cab
Siavash Hekmatnia <siavash.hekmatnia@gmail.com>
2023-06-27 18:27:51 +0330

    created restauran service domain events

---
+++ restaurant-service/restaurant-domain/restaurant-domain-core/src/main/java/com/food/ordering/system/restaurant/service/domain/event/OrderApprovedEvent.java
@@ -0,0 +1,26 @@
+package com.food.ordering.system.restaurant.service.domain.event;
+
+import com.food.ordering.system.domain.event.publisher.DomainEventPublisher;
+import com.food.ordering.system.domain.valueobject.RestaurantId;
+import com.food.ordering.system.restaurant.service.domain.entity.OrderApproval;
+
+import java.time.ZonedDateTime;
+import java.util.Collections;
+import java.util.List;
+
+public class OrderApprovedEvent extends OrderApprovalEvent {
+
+    private final DomainEventPublisher<OrderApprovedEvent> orderApprovedEventDomainEventPublisher;
+
+    public OrderApprovedEvent(OrderApproval orderApproval, RestaurantId restaurantId,
+                              ZonedDateTime createdAt,
+                              DomainEventPublisher<OrderApprovedEvent> orderApprovedEventDomainEventPublisher) {
+        super(orderApproval, restaurantId, Collections.emptyList(), createdAt);
+        this.orderApprovedEventDomainEventPublisher = orderApprovedEventDomainEventPublisher;
+    }
+
+    @Override
+    public void fire() {
+        orderApprovedEventDomainEventPublisher.publish(this);
+    }
+}
