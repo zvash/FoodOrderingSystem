7c2c9fef29075e4d869081c6c8d3e2eae7c1959e
Siavash Hekmatnia <siavash.hekmatnia@gmail.com>
2023-06-30 16:20:40 +0330

    completed the implementation of RestaurantApprovalResponseMessageListenerImpl(gets called by a kafka consumer) with by injecting OrderApprovalSaga

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/PaymentResponseMessageListenerImpl.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/PaymentResponseMessageListenerImpl.java
@@ -5,14 +5,14 @@ import com.food.ordering.system.order.service.domain.dto.message.PaymentResponse
 import com.food.ordering.system.order.service.domain.event.OrderPaidEvent;
 import com.food.ordering.system.order.service.domain.ports.input.message.listener.payment.PaymentResponseMessageListener;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
 import static com.food.ordering.system.order.service.domain.entity.Order.FAILURE_MESSAGE_DELIMITER;
 
 @Slf4j
 @Validated
-@Component
+@Service
 public class PaymentResponseMessageListenerImpl implements PaymentResponseMessageListener {
 
     private final OrderPaymentSaga orderPaymentSaga;
--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/RestaurantApprovalResponseMessageListenerImpl.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/RestaurantApprovalResponseMessageListenerImpl.java
@@ -1,18 +1,38 @@
 package com.food.ordering.system.order.service.domain;
 
 import com.food.ordering.system.order.service.domain.dto.message.RestaurantApprovalResponse;
+import com.food.ordering.system.order.service.domain.event.OrderCancelledEvent;
 import com.food.ordering.system.order.service.domain.ports.input.message.listener.restauarantapproval.RestaurantApprovalResponseMessageListener;
-import org.springframework.stereotype.Component;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
 
-@Component
+import static com.food.ordering.system.order.service.domain.entity.Order.FAILURE_MESSAGE_DELIMITER;
+
+@Slf4j
+@Validated
+@Service
 public class RestaurantApprovalResponseMessageListenerImpl implements RestaurantApprovalResponseMessageListener {
+
+    private final OrderApprovalSaga orderApprovalSaga;
+
+    public RestaurantApprovalResponseMessageListenerImpl(OrderApprovalSaga orderApprovalSaga) {
+        this.orderApprovalSaga = orderApprovalSaga;
+    }
+
     @Override
     public void orderApproved(RestaurantApprovalResponse restaurantApprovalResponse) {
-
+        orderApprovalSaga.process(restaurantApprovalResponse);
+        log.info("Order is approved for order with id: {}", restaurantApprovalResponse.getOrderId());
     }
 
     @Override
     public void orderRejected(RestaurantApprovalResponse restaurantApprovalResponse) {
-
+        OrderCancelledEvent orderCancelledEvent = orderApprovalSaga.rollback(restaurantApprovalResponse);
+        log.info("Publishing order cancelled event for order with id: {} with failure messages: {}",
+                restaurantApprovalResponse.getOrderId(),
+                String.join(FAILURE_MESSAGE_DELIMITER, restaurantApprovalResponse.getFailureMessages())
+        );
+        orderCancelledEvent.fire();
     }
 }
