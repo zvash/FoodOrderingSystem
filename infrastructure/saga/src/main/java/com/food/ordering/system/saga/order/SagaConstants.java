29dcb0fdac5b7842a99a813b2176c59f7a86af85
Siavash Hekmatnia <siavash.hekmatnia@gmail.com>
2023-07-04 21:17:15 +0330

    added a saga constants class to put saga related constants in a single place

---
+++ infrastructure/saga/src/main/java/com/food/ordering/system/saga/order/SagaConstants.java
@@ -0,0 +1,9 @@
+package com.food.ordering.system.saga.order;
+
+public final class SagaConstants {
+
+    public static final String ORDER_SAGA_NAME = "OrderProcessingSaga";
+
+    private SagaConstants() {
+    }
+}
