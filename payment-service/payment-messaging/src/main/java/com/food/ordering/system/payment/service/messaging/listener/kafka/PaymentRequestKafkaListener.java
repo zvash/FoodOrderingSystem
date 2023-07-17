6751164be48f092a651646524f3b033f98055c3a
Siavash Hekmatnia <siavash.hekmatnia@gmail.com>
2023-07-17 16:49:42 +0330

    made exception handling more accurate by importing postgressql and inspect exceptions that are caused by db

--- payment-service/payment-messaging/src/main/java/com/food/ordering/system/payment/service/messaging/listener/kafka/PaymentRequestKafkaListener.java
+++ payment-service/payment-messaging/src/main/java/com/food/ordering/system/payment/service/messaging/listener/kafka/PaymentRequestKafkaListener.java
@@ -3,17 +3,21 @@ package com.food.ordering.system.payment.service.messaging.listener.kafka;
 import com.food.ordering.system.kafka.consumer.KafkaConsumer;
 import com.food.ordering.system.kafka.order.avro.model.PaymentOrderStatus;
 import com.food.ordering.system.kafka.order.avro.model.PaymentRequestAvroModel;
+import com.food.ordering.system.payment.service.domain.exception.PaymentApplicationServiceException;
+import com.food.ordering.system.payment.service.domain.exception.PaymentDomainException;
 import com.food.ordering.system.payment.service.domain.exception.PaymentNotFoundException;
 import com.food.ordering.system.payment.service.domain.ports.input.message.listener.PaymentRequestMessageListener;
 import com.food.ordering.system.payment.service.messaging.mapper.PaymentMessagingDataMapper;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.dao.OptimisticLockingFailureException;
+import org.postgresql.util.PSQLState;
+import org.springframework.dao.DataAccessException;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
+import java.sql.SQLException;
 import java.util.List;
 
 @Slf4j
@@ -61,10 +65,20 @@ public class PaymentRequestKafkaListener implements KafkaConsumer<PaymentRequest
                             )
                     );
                 }
-            } catch (OptimisticLockingFailureException e) {
-                //NO-OP
-                log.error("Caught optimistic locking exception in PaymentRequestKafkaListener for order id: {}",
-                        paymentRequestAvroModel.getOrderId());
+            } catch (DataAccessException e) {
+                SQLException sqlException = (SQLException) e.getRootCause();
+                if (sqlException != null && sqlException.getSQLState() != null &&
+                        PSQLState.UNIQUE_VIOLATION.getState().equals(sqlException.getSQLState())) {
+                    //NO-OP
+                    log.error("Caught unique constraint exception with sql state: {} " +
+                                    "in PaymentRequestKafkaListener for order id: {}",
+                            sqlException.getSQLState(),
+                            paymentRequestAvroModel.getOrderId());
+                } else {
+                    throw new PaymentApplicationServiceException("Throwing DataAccessException in " +
+                            "PaymentRequestKafkaListener: " + e.getMessage(), e);
+                }
+
             } catch (PaymentNotFoundException e) {
                 //NO-OP
                 log.error("No payment found for order id: {}", paymentRequestAvroModel.getOrderId());
