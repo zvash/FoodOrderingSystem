f799d4e4a2e550ff2505009b0b9e9edfcc43ed9e
Siavash Hekmatnia <siavash.hekmatnia@gmail.com>
2023-07-18 20:27:15 +0330

    handle db errors and prevent retries if necessary

--- restaurant-service/restaurant-messaging/src/main/java/com/food/ordering/system/restaurant/service/messaging/listener/kafka/RestaurantApprovalRequestKafkaListener.java
+++ restaurant-service/restaurant-messaging/src/main/java/com/food/ordering/system/restaurant/service/messaging/listener/kafka/RestaurantApprovalRequestKafkaListener.java
@@ -1,16 +1,21 @@
 package com.food.ordering.system.restaurant.service.messaging.listener.kafka;
 
+import com.food.ordering.system.kafka.consumer.KafkaConsumer;
 import com.food.ordering.system.kafka.order.avro.model.RestaurantApprovalRequestAvroModel;
+import com.food.ordering.system.restaurant.service.domain.exception.RestaurantApplicationServiceException;
+import com.food.ordering.system.restaurant.service.domain.exception.RestaurantNotFoundException;
 import com.food.ordering.system.restaurant.service.domain.ports.input.message.listener.RestaurantApprovalRequestMessageListener;
 import com.food.ordering.system.restaurant.service.messaging.mapper.RestaurantMessagingDataMapper;
-import com.food.ordering.system.kafka.consumer.KafkaConsumer;
 import lombok.extern.slf4j.Slf4j;
+import org.postgresql.util.PSQLState;
+import org.springframework.dao.DataAccessException;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
+import java.sql.SQLException;
 import java.util.List;
 
 @Slf4j
@@ -43,13 +48,35 @@ public class RestaurantApprovalRequestKafkaListener implements KafkaConsumer<Res
                 offsets.toString());
 
         messages.forEach(restaurantApprovalRequestAvroModel -> {
-            log.info("Processing order approval for order id: {}",
-                    restaurantApprovalRequestAvroModel.getOrderId());
-            restaurantApprovalRequestMessageListener.approveOrder(
-                    restaurantMessagingDataMapper.restaurantApprovalRequestAvroModelToRestaurantApprovalRequest(
-                            restaurantApprovalRequestAvroModel
-                    )
-            );
+            try {
+                log.info("Processing order approval for order id: {}",
+                        restaurantApprovalRequestAvroModel.getOrderId());
+                restaurantApprovalRequestMessageListener.approveOrder(
+                        restaurantMessagingDataMapper.restaurantApprovalRequestAvroModelToRestaurantApprovalRequest(
+                                restaurantApprovalRequestAvroModel
+                        )
+                );
+            } catch (DataAccessException e) {
+                SQLException sqlException = (SQLException) e.getRootCause();
+                if (sqlException != null && sqlException.getSQLState() != null &&
+                        PSQLState.UNIQUE_VIOLATION.getState().equals(sqlException.getSQLState())) {
+                    //NO-OP
+                    log.error("Caught unique constraint exception with sql state: {} " +
+                                    "in RestaurantApprovalRequestKafkaListener for order id: {}",
+                            sqlException.getSQLState(),
+                            restaurantApprovalRequestAvroModel.getOrderId());
+                } else {
+                    throw new RestaurantApplicationServiceException("Throwing DataAccessException in " +
+                            "RestaurantApprovalRequestKafkaListener: " + e.getMessage(), e);
+                }
+
+            } catch (RestaurantNotFoundException e) {
+                //NO-OP
+                log.error("No restaurant found for restaurant id: {} and order id: {}",
+                        restaurantApprovalRequestAvroModel.getRestaurantId(),
+                        restaurantApprovalRequestAvroModel.getOrderId()
+                );
+            }
         });
 
     }
