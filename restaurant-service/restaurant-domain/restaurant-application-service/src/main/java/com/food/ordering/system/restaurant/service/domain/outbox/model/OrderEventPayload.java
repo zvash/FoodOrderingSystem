f28043b6ac4a6a4b580aa67b8af597fd0f991be4
Siavash Hekmatnia <siavash.hekmatnia@gmail.com>
2023-07-18 19:55:16 +0330

    removed extra fields from order event payload

--- restaurant-service/restaurant-domain/restaurant-application-service/src/main/java/com/food/ordering/system/restaurant/service/domain/outbox/model/OrderEventPayload.java
+++ restaurant-service/restaurant-domain/restaurant-application-service/src/main/java/com/food/ordering/system/restaurant/service/domain/outbox/model/OrderEventPayload.java
@@ -15,16 +15,10 @@ import java.util.List;
 public class OrderEventPayload {
 
     @JsonProperty
-    private String paymentId;
-
-    @JsonProperty
-    private String customerId;
-
-    @JsonProperty
     private String orderId;
 
     @JsonProperty
-    private BigDecimal price;
+    private String restaurantId;
 
     @JsonProperty
     private ZonedDateTime createdAt;
